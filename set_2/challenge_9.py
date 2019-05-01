87b509001edb2bbe99c46a59a65c4111bb04c0ef
mikeyPower <powerm3@tcd.ie>
2019-05-01 12:49:57 +0000

    challenge 9 done

--- set_2/challenge_9.py
+++ set_2/challenge_9.py
@@ -13,8 +13,26 @@ So: pad any block to a specific block length, by appending the number of bytes o
 '''
 
 def main():
+    text_to_pad = input("Enter text to pad : ")
+
+    no_of_bytes = int(input("Enter Block Size : "))
+
+    current_block_size = len(text_to_pad)
+
+    if(current_block_size > no_of_bytes):
+        print("Current string is larger than block size")
+    elif (current_block_size == no_of_bytes):
+        padding_bytes = no_of_bytes
+
+    else:
+        padding_bytes = no_of_bytes % current_block_size
 
 
+    padding = str(hex(padding_bytes))
+   # pad = padding.replace("0x","\\x0")
+    for i in range(int(padding_bytes)):
+        text_to_pad += padding
 
+    print(text_to_pad)
 if __name__== '__main__':
     main()
